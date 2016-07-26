db04cf56275967959188cfd38abe5596f125ec1f
bsergent <bsergent21897@gmail.com>
2016-07-26 15:45:22 -0400

    Offhand Consumption
    +Eating meals in the offhand now works correctly

--- src/com/sergenttech/plugins/threesquaremeals/ThreeSquareMeals.java
+++ src/com/sergenttech/plugins/threesquaremeals/ThreeSquareMeals.java
@@ -17,6 +17,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -236,16 +237,23 @@ public class ThreeSquareMeals extends JavaPlugin {
                 lore.set(1, ChatColor.GRAY + "" + (portionsValue) + " portion(s)");
                 
                 if (portionsValue <= 0) {
-                    //e.getPlayer().sendMessage(e.getItem().toString());
-                    //e.getPlayer().sendMessage(e.getPlayer().getInventory().getItemInOffHand().toString());
-                    //e.getPlayer().getInventory().setItem(e.getPlayer().getInventory().first(e.getItem()), new ItemStack(Material.BOWL));
-                    e.getPlayer().getInventory().setItemInMainHand(new ItemStack(Material.BOWL));
-                    // TODO Make this compatible with eating in the offhand
+                    PlayerInventory inv = e.getPlayer().getInventory();
+                    if (inv.getItemInMainHand().isSimilar(is)) {
+                        e.getPlayer().getInventory().setItemInMainHand(new ItemStack(Material.BOWL));
+                    } else {
+                        e.getPlayer().getInventory().setItemInOffHand(new ItemStack(Material.BOWL));
+                    }
                 } else {
-                    im.setLore(lore);
-                    is.setItemMeta(im);
-                    //e.getPlayer().getInventory().setItem(e.getPlayer().getInventory().first(e.getItem()), is);
-                    e.getPlayer().getInventory().setItemInMainHand(is);
+                    PlayerInventory inv = e.getPlayer().getInventory();
+                    if (inv.getItemInMainHand().isSimilar(is)) {
+                        im.setLore(lore);
+                        is.setItemMeta(im);
+                        e.getPlayer().getInventory().setItemInMainHand(is);
+                    } else {
+                        im.setLore(lore);
+                        is.setItemMeta(im);
+                        e.getPlayer().getInventory().setItemInOffHand(is);
+                    }
                 }
             }
             
